fce0add087a5fa4519efe06edca3160ed7b86999
Kyaw Zin Thet <kyawzin36900@gmail.com>
2024-06-04 19:29:09 +0630

    Make Design for Project Flow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Alert } from "flowbite-react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Alert color="success" onDismiss={() => alert("Alert dismissed!")}>
+        <span className="font-medium">Info alert!</span> Change a few things up
+        and try submitting again.
+      </Alert>
+    </div>
+  );
+};
 
-export default App
+export default App;
